b70a54d7f6a6b106894b4c589ba18f7b6e49db33
wislla <wislla21@gmail.com>
2021-07-09 14:46:55 -0300

    a

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,8 @@ require('dotenv/config');
 module.exports = {  
     dialect: 'mysql',
     host: process.env.HOST, 
-    username: 'root', 
-    password: '',
+    username: process.env.USERNAME, 
+    password: process.env.PASS,
     database: process.env.DATABASE,
     define:{
         timestamps: true,
